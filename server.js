c4f1198846fd9b1e1e025625ee37d03440832fea
Eric Roth <ericroth2@delta.edu>
2021-11-03 22:01:04 -0400

    initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("API running"));
+app.get("/", (req, res) => res.send("API is now running"));
 
 const PORT = process.env.PORT || 5000;
 
