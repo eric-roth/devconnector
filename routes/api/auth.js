1ade634dd043dcbeb0092d9cdb908324ac6d0aba
Eric Roth <ericroth2@delta.edu>
2021-11-08 10:39:34 -0500

    added register users and auth functionality

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,80 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 //@route  GET api/auth
 //@desc   Test route
 //@access Public
-router.get("/", (req, res) => {
-  res.send("Auth route");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
 });
+
+//@route  POST api/auth
+//@desc   Authenticate user & get token
+//@access Public
+router.post(
+  "/",
+  [
+    check("email", "email is required").isEmail(),
+    check("password", "Password is required").exists({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Desturcture request.body
+    const { email, password } = req.body;
+
+    try {
+      //See if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      // res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
+
 //connect database
 connectDB();
+
+//init Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("API is now running"));
 
